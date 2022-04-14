73ee3622a7417c9d0af8395c2b63741d40a50785
Londy D Tong-Lee <97702400+ltonglee3104@users.noreply.github.com>
2022-04-13 20:18:39 -1000

    Landing 1 (#13)
    * Landing Work
    
    Updated the home page. Need to make a new image for Logo possibly
    
    * Updated Landing backround

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,15 +3,14 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingBottom: '10px', paddingTop: '10px' };
     return (
-      <div className='landing-green-background' style={divStyle}>
+      <div className='landing-green-background'>
         <footer>
           <div className="ui center aligned container">
-              The Bowfolios Project <br />
+              Rainbow Warrior Connection Project <br />
               University of Hawaii<br />
               Honolulu, HI 96822 <br />
-            <a style={{ color: 'white' }} href="https://bowfolios.github.io">https://bowfolios.github.io</a>
+            <a style={{ color: 'white' }} href="https://rainbowwarriorconnection.github.io/">https://rainbowwarriorconnection.github.io/</a>
           </div>
         </footer>
       </div>
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image, Container, Header } from 'semantic-ui-react';
+import { Grid, Icon, Container, Header } from 'semantic-ui-react';
 
 /** Renders a color-blocked static landing page. */
 class Landing extends React.Component {
@@ -9,49 +9,40 @@ class Landing extends React.Component {
         <div className='landing-green-background'>
           <Container textAlign='center'>
             <Header style={{ paddingTop: '20px', color: 'white', fontSize: '36pt' }} as='h1'>
-                Welcome to Bowfolios
+              Welcome to the Rainbow Warrior Connection!
             </Header>
             <Header style={{ paddingBottom: '20px', color: 'white' }} as='h3'>
-                Profiles, projects, and interest areas for the UH Community
+              Browse jobs from companies, students looking for jobs, and more!
             </Header>
           </Container>
         </div>
-        <div className='landing-white-background'>
-          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>Start by making your profile....</Header>
-          <Grid container stackable columns='equal' textAlign='center'>
-            <Grid.Column>
-              <Image src="/images/home-page.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/profiles-page.png"/>
-            </Grid.Column>
-          </Grid>
-        </div>
-        <div className='landing-green-background'>
-          <Header style={{ color: 'white' }} as='h2' textAlign='center'>...then add your projects</Header>
-          <Grid container stackable columns='equal' textAlign='center'>
-            <Grid.Column>
-              <Image src="/images/add-project-page.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/projects-page.png"/>
-            </Grid.Column>
-          </Grid>
-        </div>
-        <div className='landing-white-background'>
-          <Header style={{ color: '#376551' }} as='h2' textAlign='center'>
-              Connect to people and projects with shared interests!
-          </Header>
-          <Grid container stackable columns='equal' textAlign='center'>
-            <Grid.Column>
-              <Image src="/images/interests-page.png"/>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src="/images/filter-page.png"/>
-            </Grid.Column>
-          </Grid>
-        </div>
+        <div className='home-landing-background'>
+          <div className='transparency'>
+          <Grid style={{ paddingTop: '20px' }} container centered stackable columns={3}>
 
+            <Grid.Column textAlign='center'>
+              <Icon size="huge" name="users" style={{ color: 'white' }}/>
+              <Header as='h1' style={{ color: 'white' }}>Multiple Users</Header>
+              <Header as='h3' style={{ color: 'white' }}>This website allows UH Manoa
+                students and tech companies to create profiles and show them on our website.</Header>
+            </Grid.Column>
+
+            <Grid.Column textAlign='center'>
+              <Icon size="huge" name="address book" style={{ color: 'white' }}/>
+              <Header as='h1' style={{ color: 'white' }}>Contact Details</Header>
+              <Header as='h3' style={{ color: 'white' }}>Students are able to browse companies and their geographical location to find the
+                best job for them. Companies and browse students and post jobs on the websites to recruit students.</Header>
+            </Grid.Column>
+
+            <Grid.Column textAlign='center'>
+              <Icon size="huge" name="sitemap" style={{ color: 'white' }}/>
+              <Header as='h1' style={{ color: 'white' }}>Browse for Free</Header>
+              <Header as='h3' style={{ color: 'white' }}>Anyone can browse this website and see what
+                companies are hiring and who are looking for jobs.</Header>
+            </Grid.Column>
+          </Grid>
+          </div>
+        </div>
       </div>
 
     );
