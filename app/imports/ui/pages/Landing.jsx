a4a1ad195ca082b5b8caf63d2dc452f523894690
Londy D Tong-Lee <ltonglee@hawaii.edu>
2022-04-19 10:50:01 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -18,29 +18,29 @@ class Landing extends React.Component {
         </div>
         <div className='home-landing-background'>
           <div className='transparency'>
-          <Grid style={{ paddingTop: '20px' }} container centered stackable columns={3}>
+            <Grid style={{ paddingTop: '20px' }} container centered stackable columns={3}>
 
-            <Grid.Column textAlign='center'>
-              <Icon size="huge" name="users" style={{ color: 'white' }}/>
-              <Header as='h1' style={{ color: 'white' }}>Multiple Users</Header>
-              <Header as='h3' style={{ color: 'white' }}>This website allows UH Manoa
-                students and tech companies to create profiles and show them on our website.</Header>
-            </Grid.Column>
+              <Grid.Column textAlign='center'>
+                <Icon size="huge" name="users" style={{ color: 'white' }}/>
+                <Header as='h1' style={{ color: 'white' }}>Multiple Users</Header>
+                <Header as='h3' style={{ color: 'white' }}>This website allows UH Manoa
+                  students and tech companies to create profiles and show them on our website.</Header>
+              </Grid.Column>
 
-            <Grid.Column textAlign='center'>
-              <Icon size="huge" name="address book" style={{ color: 'white' }}/>
-              <Header as='h1' style={{ color: 'white' }}>Contact Details</Header>
-              <Header as='h3' style={{ color: 'white' }}>Students are able to browse companies and their geographical location to find the
-                best job for them. Companies and browse students and post jobs on the websites to recruit students.</Header>
-            </Grid.Column>
+              <Grid.Column textAlign='center'>
+                <Icon size="huge" name="address book" style={{ color: 'white' }}/>
+                <Header as='h1' style={{ color: 'white' }}>Contact Details</Header>
+                <Header as='h3' style={{ color: 'white' }}>Students are able to browse companies and their geographical location to find the
+                  best job for them. Companies and browse students and post jobs on the websites to recruit students.</Header>
+              </Grid.Column>
 
-            <Grid.Column textAlign='center'>
-              <Icon size="huge" name="sitemap" style={{ color: 'white' }}/>
-              <Header as='h1' style={{ color: 'white' }}>Browse for Free</Header>
-              <Header as='h3' style={{ color: 'white' }}>Anyone can browse this website and see what
-                companies are hiring and who are looking for jobs.</Header>
-            </Grid.Column>
-          </Grid>
+              <Grid.Column textAlign='center'>
+                <Icon size="huge" name="sitemap" style={{ color: 'white' }}/>
+                <Header as='h1' style={{ color: 'white' }}>Browse for Free</Header>
+                <Header as='h3' style={{ color: 'white' }}>Anyone can browse this website and see what
+                  companies are hiring and who are looking for jobs.</Header>
+              </Grid.Column>
+            </Grid>
           </div>
         </div>
       </div>
