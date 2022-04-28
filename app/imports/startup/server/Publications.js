2ae695485871ab240f09861d925933ec9d125e1b
Bret Witt <bretwitt@hawaii.edu>
2022-04-28 11:28:55 -1000

    Students Interests & Interests collections (#51)

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,9 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import { Students } from '../../api/students/Students';
 import { Companies } from '../../api/companies/Companies';
+import { StudentsInterests } from '../../api/students/StudentsInterest'
+import { Interests } from '../../api/interests/Interests'
 
 Meteor.publish(Students.userPublicationName, () => Students.collection.find());
 Meteor.publish(Companies.userPublicationName, () => Companies.collection.find());
+Meteor.publish(Interests.userPublicationName, () => Interests.collection.find());
+Meteor.publish(StudentsInterests.userPublicationName, () => StudentsInterests.collection.find());
 
 // alanning:roles publication
 // Recommended code to publish roles for each user.
