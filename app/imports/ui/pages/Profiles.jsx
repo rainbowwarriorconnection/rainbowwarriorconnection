2c9ed1ef02974dcafe06c4aa8a07bd87033c9435
Bret Witt <bretwitt@hawaii.edu>
2022-04-30 19:20:14 -1000

    Interests mapping to profile cards

--- app/imports/ui/pages/Profiles.jsx
+++ app/imports/ui/pages/Profiles.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Loader, Card, Image } from 'semantic-ui-react';
+import { Container, Loader, Card, Image, Header, Label } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
 import { Students } from '../../api/students/Students';
-
+import { StudentsInterests } from '../../api/students/StudentsInterest';
 /** Returns the Profile and associated Projects and Interests associated with the passed user email. */
 function getProfileData(email) {
   const data = Students.collection.findOne({ email });
-  return _.extend({ }, data);
+  const interests = _.pluck(StudentsInterests.collection.find({ email }).fetch(), 'interest');
+  return _.extend({ }, data, { interests: interests } );
 }
 
 /** Component for layout out a Profile Card. */
@@ -22,6 +23,9 @@ const MakeCard = (props) => (
     </Card.Content>
     <Card.Content>
       {props.profile.description}
+    </Card.Content>
+    <Card.Content>
+       { _.map(props.profile.interests, (interest, index) => <Label key={index}>{interest}</Label>)}
     </Card.Content>
   </Card>
 );
@@ -42,6 +46,7 @@ class ProfilesPage extends React.Component {
   renderPage() {
     const emails = _.pluck(Students.collection.find().fetch(), 'email');
     const studentData = emails.map(email => getProfileData(email));
+    console.log(studentData)
     return (
       <Container id="student-profiles-page">
         <Card.Group>
@@ -60,7 +65,8 @@ ProfilesPage.propTypes = {
 export default withTracker(() => {
   // Ensure that minimongo is populated with all collections prior to running render().
   const sub1 = Meteor.subscribe(Students.userPublicationName);
+  const sub2 = Meteor.subscribe(StudentsInterests.userPublicationName);
   return {
-    ready: sub1.ready(),
+    ready: sub1.ready() && sub2.ready(),
   };
 })(ProfilesPage);
