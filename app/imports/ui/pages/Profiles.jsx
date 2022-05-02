8bb62c93a4de1e338137aba4f78c936615729bcf
Bret Witt <bretwitt@hawaii.edu>
2022-05-01 21:30:25 -1000

    Filtering component (#60)

--- app/imports/ui/pages/Profiles.jsx
+++ app/imports/ui/pages/Profiles.jsx
@@ -6,6 +6,9 @@ import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
 import { Students } from '../../api/students/Students';
 import { StudentsInterests } from '../../api/students/StudentsInterest';
+import { Interests } from '../../api/interests/Interests';
+import FilterBar from '../components/FilterBar';
+
 /** Returns the Profile and associated Projects and Interests associated with the passed user email. */
 function getProfileData(email) {
   const data = Students.collection.findOne({ email });
@@ -37,20 +40,48 @@ MakeCard.propTypes = {
 /** Renders the Profile Collection as a set of Cards. */
 class ProfilesPage extends React.Component {
 
+
+  constructor(props) {
+     super(props)
+     this.state = {
+	filterType: "search",
+	filterValue: ""
+     }
+  }
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
+  onFilter = (filterType, val) => {
+      this.setState({
+	filterType: filterType,
+	filterValue: val
+      })
+  }
+
+  renderCard(profile, index) {
+    const val = this.state.filterValue
+    if(!val) {
+       return <MakeCard key={index} profile={profile}/>
+    }
+    if(this.state.filterType == "search") {
+       const fullName = profile.firstName.toUpperCase() + " " + profile.lastName.toUpperCase();
+       return (fullName.indexOf(val.toUpperCase()) > -1) ? <MakeCard key={index} profile={profile}/> : ""
+    } else {
+       return (profile.interests.includes(val)) ? <MakeCard key={index} profile={profile}/> : ""
+    }
+  }
   /** Render the page once subscriptions have been received. */
   renderPage() {
     const emails = _.pluck(Students.collection.find().fetch(), 'email');
     const studentData = emails.map(email => getProfileData(email));
-    console.log(studentData);
+    const interests = _.pluck(Interests.collection.find().fetch(), 'name');
     return (
       <Container>
+	<FilterBar onFilter={this.onFilter} interests={interests}/>
         <Card.Group className="card-format" centered>
-          {_.map(studentData, (profile, index) => <MakeCard key={index} profile={profile}/>)}
+          {_.map(studentData, (profile, index) => this.renderCard(profile, index))}
         </Card.Group>
       </Container>
     );
@@ -66,7 +97,8 @@ export default withTracker(() => {
   // Ensure that minimongo is populated with all collections prior to running render().
   const sub1 = Meteor.subscribe(Students.userPublicationName);
   const sub2 = Meteor.subscribe(StudentsInterests.userPublicationName);
+  const sub3 = Meteor.subscribe(Interests.userPublicationName);
   return {
-    ready: sub1.ready() && sub2.ready(),
+    ready: sub1.ready() && sub2.ready() && sub3.ready(),
   };
 })(ProfilesPage);
